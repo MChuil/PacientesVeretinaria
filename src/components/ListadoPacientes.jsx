7a315f39f8dafd080cf26b608a7e7cff566fce6b
Miguel Chuil <developer.chuil@gmail.com>
2022-10-02 22:46:24 -0500

    Iniciando el proyecto

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,43 @@
+const ListadoPacientes = () => {
+  return (
+    <div className="w-1/2 lg:w-3/5">
+      <h2 className="text-center text-3xl font-bold">Listado de Pacientes</h2>
+      <p className="text-lg mt-5 mb-10 text-center">
+        Administra tus {''}
+        <span className="font-bold text-indigo-600">Pacientes y Citas</span>
+      </p>
+      <div className="mt-3 bg-white shadow-sm p-10 rounded-lg">
+        <p className="text-gray-700 font-bold uppercase">
+          Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="text-gray-700 font-bold uppercase">
+          Propietario: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="text-gray-700 font-bold uppercase">
+          Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="text-gray-700 font-bold uppercase">
+          Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="text-gray-700 font-bold uppercase">
+          Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="text-gray-700 font-bold uppercase">
+          Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="text-gray-700 font-bold uppercase">
+          Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ListadoPacientes
